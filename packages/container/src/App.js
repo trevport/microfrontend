ca4e9f6149dd7833c7321c8545b5d9dfe3691773
Trevor Porter <tporter@signiant.com>
2022-02-28 18:06:14 -0800

    Fix CSS issues by namespacing CSS rules

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,22 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
-// force build
+const generateClassName = createGenerateClassName({ productionPrefix: "ct" });
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
